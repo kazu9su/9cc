d9fedc387ffc240b0bcd8dfc2986e68784419ae4
tommy <kazu9su@gmail.com>
2019-09-12 03:24:03 +0000

    error at

--- 9cc.c
+++ 9cc.c
@@ -33,6 +33,21 @@ void error(char *fmt, ...) {
     exit(1);
 }
 
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 // 次のトークンが期待している記号のときにはトークンを1つ読み進めて
 // 真を返す。それ以外のときには偽を返す
 bool consume(char op) {
@@ -44,13 +59,13 @@ bool consume(char op) {
 
 void expect(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     token = token->next;
 }
 
 int expect_number() {
     if (token->kind != TK_NUM)
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
@@ -105,8 +120,10 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    user_input = argv[1];
+
     // トークナイズする
-    token = tokenize(argv[1]);
+    token = tokenize(user_input);
 
     // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
